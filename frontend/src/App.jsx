8da6b8bd4a35e625d9f9e1882872ffa553ea9891
Kartik Turak <kartikturak1@gmail.com>
2024-12-04 13:59:43 +0530

    Banner Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import NavBar from './Components/NavBar'
 import Landing from './components/Landing'
+import Banner from './components/Banner'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
     <>
       <NavBar/>
       <Landing/>
+      <Banner/>
     </>
   )
 }
--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 function Banner() {
   return (
     <>
-        
+        <div className='mt-10 bg-slate-200 '>
+            <div>
+                <h1 className='text-center text-indigo-800 font-semibold  text-3xl pt-5 pb-5'>A sneak peak into the digital experience</h1>
+            </div>
+
+            <div className="image m-20">
+                <img src="https://t4.ftcdn.net/jpg/04/73/38/27/360_F_473382708_J1CcBu3ylkyjWTjxK66Q4tmgRcF9lGU1.jpg" alt="" className='rounded-3xl w-screen h-screen ml-5 mr-5 mb-5 '/>
+            </div>
+        </div>
     </>
   )
 }
